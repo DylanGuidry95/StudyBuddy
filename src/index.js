85ac0e7355bff14531cc116886f0316ce97d335f
Dylan Guidry <guidry.dylan.95@gmail.com>
2026-01-16 14:07:12 -0600

    fix base navigation

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   
     <React.StrictMode>
       <AuthProvider>
-        <BrowserRouter basename="StudyBuddy">
+        <BrowserRouter basename="/StudyBuddy">
           <App />
         </BrowserRouter>
       </AuthProvider>
